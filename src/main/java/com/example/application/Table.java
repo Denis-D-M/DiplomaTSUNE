1b205e9a9fdb12abd02d949a41189b10846a59c2
Denis Mishin <denism@humanforce.com>
2022-05-03 00:36:38 +0400

    Done all the steps of adding information

--- src/main/java/com/example/application/Table.java
+++ src/main/java/com/example/application/Table.java
@@ -1,16 +1,25 @@
 package com.example.application;
 
 import com.vaadin.flow.component.button.Button;
-import java.util.ArrayList;
-import java.util.List;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextField;
 
 public class Table {
 
-  private final int num;
-  private final boolean isFree;
+  private int num;
+  private boolean isFree;
   private final Button button;
-  private final String bookedBy;
-  private final List<String> equip;
+  private String bookedBy;
+
+  private Dialog dialog = new Dialog();
+
+  private FormLayout formLayout = new FormLayout();
+
+  private TextField numberField = new TextField("Workplace number");
+
+  private Select<Employee> bookedByField;
 
   public Table(int num, boolean isFree, String bookedBy) {
     this.num = num;
@@ -18,7 +27,7 @@ public class Table {
     this.button = new Button(String.valueOf(num));
     this.button.setClassName("big-button");
     this.bookedBy = bookedBy;
-    this.equip = new ArrayList<>();
+    this.dialog.setHeight("35%");
   }
 
   public int getNum() {
@@ -37,7 +46,35 @@ public class Table {
     return bookedBy;
   }
 
-  public List<String> getEquip() {
-    return equip;
+  public Dialog getDialog() {
+    return dialog;
+  }
+
+  public FormLayout getFormLayout() {
+    return formLayout;
+  }
+
+  public TextField getNumberField() {
+    return numberField;
+  }
+
+  public Select<Employee> getBookedByField() {
+    return bookedByField;
+  }
+
+  public void setNum(int num) {
+    this.num = num;
+  }
+
+  public void setFree(boolean free) {
+    isFree = free;
+  }
+
+  public void setBookedBy(String bookedBy) {
+    this.bookedBy = bookedBy;
+  }
+
+  public void setBookedByField(Select<Employee> bookedByField) {
+    this.bookedByField = bookedByField;
   }
 }
--- src/main/java/com/example/application/views/list/MainView.java
+++ src/main/java/com/example/application/views/list/MainView.java
@@ -1,7 +1,9 @@
 package com.example.application.views.list;
 
+import com.example.application.Employee;
 import com.example.application.Floor;
 import com.example.application.Room;
+import com.example.application.Table;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dialog.Dialog;
@@ -9,10 +11,12 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import java.util.List;
+import java.util.Objects;
 
 @CssImport("custom-styles.css")
 @PageTitle("АИС Haulmont")
@@ -25,12 +29,20 @@ public class MainView extends VerticalLayout {
   private final H1 floor = new H1("FLOOR");
 
   private final H1 room = new H1("ROOM");
+  private final H1 workplace = new H1("WORKPLACE");
 
   private final Button addRoomButton = new Button("ADD");
+  private final Button deleteRoomButton = new Button("DELETE");
+
+  private final Button addWorkplaceButton = new Button("ADD");
+  private final Button deleteWorkplaceButton = new Button("DELETE");
 
   private final List<Floor> floors = List.of(new Floor(1), new Floor(2), new Floor(3));
 
   private int currFloor;
+  private Room currRoom;
+
+  private boolean isDeleteActive = false;
 
   public MainView() {
     configureClasses();
@@ -47,7 +59,11 @@ public class MainView extends VerticalLayout {
     the.setClassName("the");
     floor.setClassName("floor");
     room.setClassName("room");
+    workplace.setClassName("room");
     addRoomButton.setClassName("add-button");
+    deleteRoomButton.setClassName("delete-button");
+    addWorkplaceButton.setClassName("add-button");
+    deleteWorkplaceButton.setClassName("delete-button");
     configureListeners();
     menu.add(choose, the, floor);
     menu.add(floors.get(0).getButton(), floors.get(1).getButton(), floors.get(2).getButton());
@@ -59,22 +75,28 @@ public class MainView extends VerticalLayout {
       removeAllFloors();
       menu.add(room);
       menu.add(addRoomButton);
+      menu.add(deleteRoomButton);
       showRooms(1);
     });
+
     floors.get(1).getButton().addClickListener(buttonClickEvent -> {
       currFloor = 1;
       removeAllFloors();
       menu.add(room);
       menu.add(addRoomButton);
+      menu.add(deleteRoomButton);
       showRooms(2);
     });
+
     floors.get(2).getButton().addClickListener(buttonClickEvent -> {
       currFloor = 2;
       removeAllFloors();
       menu.add(room);
       menu.add(addRoomButton);
+      menu.add(deleteRoomButton);
       showRooms(3);
     });
+
     addRoomButton.addClickListener(buttonClickEvent -> {
       var dialog = new Dialog();
       var formLayout = new FormLayout();
@@ -85,10 +107,23 @@ public class MainView extends VerticalLayout {
       addButton.setClassName("add-button");
       addButton.addClickListener(buttonClickEvent1 -> {
         floors.get(currFloor).getRooms().add(new Room(Integer.parseInt(numberField.getValue())));
+        floors.get(currFloor).getRooms()
+            .forEach(aRoom -> aRoom.getButton().addClickListener(aButtonEvent -> {
+              currRoom = aRoom;
+              if (isDeleteActive) {
+                aRoom.getButton().setClassName("big-button-delete-stub");
+                return;
+              }
+              removeAllRooms();
+              menu.add(workplace);
+              menu.remove(addRoomButton);
+              menu.remove(deleteRoomButton);
+              menu.add(addWorkplaceButton);
+              menu.add(deleteWorkplaceButton);
+            }));
         showRooms(currFloor);
         dialog.close();
       });
-
       dialog.add(formLayout, addButton);
       dialog.open();
       menu.remove(floors.get(0).getButton());
@@ -96,16 +131,109 @@ public class MainView extends VerticalLayout {
       menu.remove(floors.get(2).getButton());
     });
 
+    deleteRoomButton.addClickListener(buttonClickEvent -> {
+      isDeleteActive = !isDeleteActive;
+      for (var iterator = floors.get(currFloor).getRooms().iterator(); iterator.hasNext(); ) {
+        var aRoom = iterator.next();
+        var roomButton = aRoom.getButton();
+        if (isDeleteActive) {
+          roomButton.setClassName("big-button-delete");
+        } else {
+          if ("big-button-delete-stub".equals(aRoom.getButton().getClassName())) {
+            menu.remove(roomButton);
+            iterator.remove();
+          }
+          roomButton.setClassName("big-button");
+        }
+      }
+    });
+
+    floors.get(currFloor).getRooms()
+        .forEach(aRoom -> aRoom.getButton().addClickListener(aButtonEvent -> {
+          currRoom = aRoom;
+          if (isDeleteActive) {
+            aRoom.getButton().setClassName("big-button-delete-stub");
+            return;
+          }
+          removeAllRooms();
+        }));
+
+    addWorkplaceButton.addClickListener(buttonClickEvent -> {
+      var table = new Table(0, false, null);
+      var dialog = table.getDialog();
+      var formLayout = table.getFormLayout();
+      dialog.add(formLayout);
+      var numberField = table.getNumberField();
+      table.setBookedByField(new Select<>(new Employee("Denis Mishin", "Java Developer"),
+          new Employee("Nikita Ermochenko", "Junior Java Developer"),
+          new Employee("Artyom Bazanov", "Manager"),
+          new Employee("Anton Yudin", "Cleaner")));
+      var bookedByField = table.getBookedByField();
+      bookedByField.setLabel("Booked by");
+      bookedByField.setEmptySelectionAllowed(true);
+      formLayout.add(numberField);
+      formLayout.add(bookedByField);
+      var addButton = new Button("ADD");
+      addButton.setClassName("add-button");
+      var employee = bookedByField.getValue();
+      addButton.addClickListener(buttonClickEvent1 -> {
+        table.setNum(Integer.parseInt(numberField.getValue()));
+        table.setFree(Objects.isNull(employee));
+        table.getBookedByField().setValue(employee);
+        currRoom.getTables().add(table);
+        currRoom.getTables().forEach(aTable -> aTable.getButton().addClickListener(aButtonEvent -> {
+          if (isDeleteActive) {
+            aTable.getButton().setClassName("big-button-delete-stub");
+            return;
+          }
+          if (!aTable.getDialog().isOpened()) {
+            aTable.getDialog().open();
+          }
+        }));
+        showTables();
+        dialog.close();
+        dialog.remove(addButton);
+      });
+      dialog.add(formLayout, addButton);
+      dialog.open();
+      menu.remove(currRoom.getButton());
+    });
+
+    deleteWorkplaceButton.addClickListener(buttonClickEvent -> {
+      isDeleteActive = !isDeleteActive;
+      for (var iterator = currRoom.getTables().iterator(); iterator.hasNext(); ) {
+        var aTable = iterator.next();
+        var tableButton = aTable.getButton();
+        if (isDeleteActive) {
+          tableButton.setClassName("big-button-delete");
+        } else {
+          if ("big-button-delete-stub".equals(aTable.getButton().getClassName())) {
+            menu.remove(tableButton);
+            iterator.remove();
+          }
+          tableButton.setClassName("big-button");
+        }
+      }
+    });
   }
 
   private void removeAllFloors() {
     floors.forEach(aFloor -> aFloor.getButton().setClassName("big-button-stub"));
     floor.setClassName("floor-stub");
+  }
+
+  private void removeAllRooms() {
+    floors.get(currFloor).getRooms().forEach(aRoom -> aRoom.getButton().setClassName("big-button-stub"));
+    room.setClassName("floor-stub");
 
   }
 
   private void showRooms(int floor) {
     floors.get(floor).getRooms().forEach(room1 -> menu.add(room1.getButton()));
+  }
+
+  private void showTables() {
+    currRoom.getTables().forEach(aTable -> menu.add(aTable.getButton()));
   }
 
 }
